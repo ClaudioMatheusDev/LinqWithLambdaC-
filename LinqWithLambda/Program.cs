9a2834fe656b85808f23b0d6ba396c7633867920
Claudio Matheus <claudiomatheus055@gmail.com>
2024-09-23 23:34:09 -0300

    feat: add function to search products using LAMBDA and LINQ
    - Implemented a function that allows searching for products using LAMBDA expressions and LINQ for more efficient and flexible product searches.

--- LinqWithLambda/Program.cs
+++ LinqWithLambda/Program.cs
@@ -1,5 +1,16 @@
 ﻿using LinqWithLambda.Entities;
 using System.Collections.Generic;
+using System.Linq;
+
+static void Print<T>(string message, IEnumerable<T> collection)
+{
+    Console.WriteLine(message);
+    foreach(T obj in collection)
+    {
+        Console.WriteLine(obj);
+    }
+    Console.WriteLine();
+}//Função para imprimir todo resultado
 
 Category c1 = new Category() { Id = 1, Name = "Tools", Tier = 2};
 Category c2 = new Category() { Id = 2, Name = "Computers", Tier = 1};
@@ -18,3 +29,8 @@ List<Product> products = new List<Product>()
     new Product() { Id = 9,Name = "Sound Bar", Price = 700.0, Category = c2 },
     new Product() { Id = 10,Name = "Level", Price = 10.0, Category = c2 }
 };
+var r1 = products.Where(p => p.Category.Tier == 1 && p.Price < 900.00); //Adicionando um LINQ para achar todos os produtos que tem o valor menor que 900.00
+
+Print("Tier 1 AND PRICE < 900: ", r1);
+
+
